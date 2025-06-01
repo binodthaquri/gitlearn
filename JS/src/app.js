7f7d9fea0f87ca20a933007fdff799c0b6e5a2a5
binod thaqri <binodthaquri207@gmail.com>
2025-06-01 11:08:45 +0545

    all file ok

---
+++ JS/src/app.js
@@ -0,0 +1,63 @@
+console.log("welcome from js. ")
+
+// javascript runs on web brower internally.
+// alert("helo js.")
+
+// frontend scripting language. 
+
+// fetch api.
+// dynamic animation.
+// making interactive and functional. 
+// client side validation. 
+
+// name = prompt("enter your name. ")
+
+// alert("welcome " + name)
+
+// x = document.getElementById("mybtn")
+
+// $("mybtn").onclick() => {
+//     alert("hello with jqury. ")
+// }
+
+// function alertbox(){
+//     alert("hello with jqury. ")
+// }
+
+
+
+
+
+
+// print ways. 
+// alert console prompt
+// datatype in JS
+// variable - variable is a container which stores Data
+name2 = "binod thakuri"
+console.log(name2)
+
+// naming conventions in Js
+// variable name starts with text letter or underscore. 
+// 9name = "harish"
+
+
+age="age 20"
+console.log(age)
+
+addresh="bhawatimai RM dailekh"
+console.log(addresh)
+
+x = prompt("enter yoru name. ")
+console.log(x)
+
+cart = 0;
+
+
+document.getElementsById("plusbtn").addEventListener("click",()=> {
+    console.log("you clicked me. ")
+    // cart += a;
+
+    // console.log(cart)
+})
+
+
